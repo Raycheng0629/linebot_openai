e1397c9fa461296c944d55e1c3e9b5f0570e9891
Raycheng0629 <148304144+Raycheng0629@users.noreply.github.com>
2024-05-01 01:58:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def welcome(event):
 def handle_message(event):
     msg = event.message.text
     if msg == '爬蟲':
-        crawl_weather_data()
+        crawl_weather_data(event.reply_token)
     else:
         try:
             GPT_answer = GPT_response(msg)
@@ -94,21 +94,22 @@ def handle_message(event):
 
 import json
 
-def crawl_weather_data():
+def crawl_weather_data(reply_token):
     file_path = r"/content/F-C0032-001.json"
 
     with open(file_path, 'r') as file:
         data_json = json.load(file)
 
-location = data_json['cwaopendata']['dataset']['location']
-for i in location:
-    city = i['locationName']    # 縣市名稱
-    wx8 = i['weatherElement'][0]['time'][0]['parameter']['parameterName']    # 天氣現象
-    maxt8 = i['weatherElement'][1]['time'][0]['parameter']['parameterName']  # 最高溫
-    mint8 = i['weatherElement'][2]['time'][0]['parameter']['parameterName']  # 最低溫
-    ci8 = i['weatherElement'][3]['time'][0]['parameter']['parameterName']    # 舒適度
-    pop8 = i['weatherElement'][4]['time'][0]['parameter']['parameterName']   # 降雨機率
-    print(f'{city}未來 8 小時{wx8}，最高溫 {maxt8} 度，最低溫 {mint8} 度，降雨機率 {pop8} %')
+    location = data_json['cwaopendata']['dataset']['location']
+    for i in location:
+        city = i['locationName']    # 縣市名稱
+        wx8 = i['weatherElement'][0]['time'][0]['parameter']['parameterName']    # 天氣現象
+        maxt8 = i['weatherElement'][1]['time'][0]['parameter']['parameterName']  # 最高溫
+        mint8 = i['weatherElement'][2]['time'][0]['parameter']['parameterName']  # 最低溫
+        ci8 = i['weatherElement'][3]['time'][0]['parameter']['parameterName']    # 舒適度
+        pop8 = i['weatherElement'][4]['time'][0]['parameter']['parameterName']   # 降雨機率
+        message = f'{city}未來 8 小時{wx8}，最高溫 {maxt8} 度，最低溫 {mint8} 度，降雨機率 {pop8} %'
+        line_bot_api.reply_message(reply_token, TextSendMessage(message))
 
 
 #---
