b283a9fbf4be6b1be463e2a4b96906f0ffeef5c6
Raycheng0629 <148304144+Raycheng0629@users.noreply.github.com>
2024-05-03 21:43:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,90 +1,26 @@
-from flask import Flask, request, abort
-import os 
+from flask import Flask, request
+
+# 載入 LINE Message API 相關函式庫
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import *
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, StickerSendMessage, ImageSendMessage, LocationSendMessage
 
-import math, json, time, requests
-
+import json
 app = Flask(__name__)
 
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
-
-# Combine route definitions
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
 
 @app.route("/", methods=['POST'])
 def linebot():
-    body = request.get_data(as_text=True)
+    body = request.get_data(as_text=True)                 # 取得收到的訊息內容
     try:
-        line_bot_api = LineBotApi(access_token)
-        handler = WebhookHandler(channel_secret)
-        signature = request.headers['X-Line-Signature']
-        handler.handle(body, signature)
-        json_data = json.loads(body)
-        reply_token = json_data['events'][0]['replyToken']
-        user_id = json_data['events'][0]['source']['userId']
-        type = json_data['events'][0]['message']['type']
-        if type == 'text':
-            text = json_data['events'][0]['message']['text']
-            if text == '雷達回波圖' or text == '雷達回波':
-                line_bot_api.push_message(user_id, TextSendMessage(text='馬上找給你！抓取資料中....'))
-                img_url = f'https://cwaopendata.s3.ap-northeast-1.amazonaws.com/Observation/O-A0058-001.png?{time.time_ns()}'
-                img_message = ImageSendMessage(original_content_url=img_url, preview_image_url=img_url)
-                line_bot_api.reply_message(reply_token,img_message)
-            elif text == '地震':
-                line_bot_api.push_message(user_id, TextSendMessage(text='馬上找給你！抓取資料中....'))
-                reply = earth_quake()
-                text_message = TextSendMessage(text=reply[0])
-                line_bot_api.reply_message(reply_token,text_message)
-                line_bot_api.push_message(user_id, ImageSendMessage(original_content_url=reply[1], preview_image_url=reply[1]))
-            else:
-                reply = cctv(text)
-                if not reply == '':
-                    text_message = TextSendMessage(text=reply)
-                    line_bot_api.reply_message(reply_token,text_message)
-                    sec = math.ceil(time.time())
-                    reply = reply + f'snapshot?t={sec}'
-                    line_bot_api.push_message(user_id, ImageSendMessage(original_content_url=reply, preview_image_url=reply))
-                else:
-                    text_message = TextSendMessage(text=text)
-                    line_bot_api.reply_message(reply_token,text_message)
-        elif type == 'location':
-            line_bot_api.push_message(user_id, TextSendMessage(text='馬上找給你！抓取資料中....'))
-            address = json_data['events'][0]['message']['address'].replace('台','臺')  # 取出地址資訊，並將「台」換成「臺」
-            reply = weather(address)
-            text_message = TextSendMessage(text=reply)
-            line_bot_api.reply_message(reply_token,text_message)
-    except Exception as e:
-        print(e)
-    return 'OK'
-
-# Combine event handlers
-@handler.add(PostbackEvent)
-def handle_message(event):
-    print(event.postback.data)
-
-@handler.add(MemberJoinedEvent)
-def welcome(event):
-    uid = event.joined.members[0].user_id
-    gid = event.source.group_id
-    profile = line_bot_api.get_group_member_profile(gid, uid)
-    name = profile.display_name
-    message = TextSendMessage(text=f'{name}歡迎加入')
-    line_bot_api.reply_message(event.reply_token, message)
-
+        line_bot_api = LineBotApi('5P7RUl8NwthjuSPZvo8eGR0XNToFXesvPkBcJdpmvHab2pgus1PenUaO43WfO4U+hO/tzsLZHm+h+dpayZ5cwVh4XwBS+OZeB/lGwSxpoP04000GM4FcdU6553pPue5KGa/HIqybLn/OcSA/wNEfxgdB04t89/1O/w1cDnyilFU=')     # 確認 token 是否正確
+        handler = WebhookHandler('82834ecbcd9989b5e1723a193c85ee72')    # 確認 secret 是否正確
+        signature = request.headers['X-Line-Signature']   # 加入回傳的 headers
+        handler.handle(body, signature)      # 綁定訊息回傳的相關資訊
+        json_data = json.loads(body)         # 轉換內容為 json 格式
+        print(json_data)                     # 印出內容
+    except:
+        print(body)             # 如果發生錯誤，印出收到的內容
+    return 'OK'                 # 驗證 Webhook 使用，不能省略
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
