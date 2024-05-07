81158f0b47266986991770af8b886c35c36d1320
Raycheng0629 <148304144+Raycheng0629@users.noreply.github.com>
2024-05-07 15:47:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -86,87 +86,35 @@ def earth_quake():
         result = ['Failed to fetch data...', '']
     return result
 
-def weather(address):
-    result = {}
+def check(address):
     code = 'CWA-B683EE16-4F0D-4C8F-A2AB-CCCA415C60E1'
-    # 即時天氣
-    try:
-        url = [f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization={code}',
-            f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization={code}']
-        for item in url:
-            req = requests.get(item)   # 爬取目前天氣網址的資料
-            data = req.json()
-            station = data['records']['Station']
-            for i in station:
-                city = i['GeoInfo']['CountyName']
-                area = i['GeoInfo']['TownName']
-                if not f'{city}{area}' in result:
-                    weather = i['WeatherElement']['Weather']
-                    temp = i['WeatherElement']['AirTemperature']
-                    humid = i['WeatherElement']['RelativeHumidity']
-                    result[f'{city}{area}'] = f'目前天氣狀況「{weather}」，溫度 {temp} 度，相對濕度 {humid}%！'
-    except:
-        pass
-
-
-    # 氣象預報
-    api_list = {"宜蘭縣":"F-D0047-001","桃園市":"F-D0047-005","新竹縣":"F-D0047-009","苗栗縣":"F-D0047-013",
-        "彰化縣":"F-D0047-017","南投縣":"F-D0047-021","雲林縣":"F-D0047-025","嘉義縣":"F-D0047-029",
-        "屏東縣":"F-D0047-033","臺東縣":"F-D0047-037","花蓮縣":"F-D0047-041","澎湖縣":"F-D0047-045",
-        "基隆市":"F-D0047-049","新竹市":"F-D0047-053","嘉義市":"F-D0047-057","臺北市":"F-D0047-061",
-        "高雄市":"F-D0047-065","新北市":"F-D0047-069","臺中市":"F-D0047-073","臺南市":"F-D0047-077",
-        "連江縣":"F-D0047-081","金門縣":"F-D0047-085"}
-    for name in api_list:
-        if name in address:
-            city_id = api_list[name]
-    t = time.time()
-    t1 = time.localtime(t+28800)
-    t2 = time.localtime(t+28800+10800)
-    now = time.strftime('%Y-%m-%dT%H:%M:%S',t1)
-    now2 = time.strftime('%Y-%m-%dT%H:%M:%S',t2)
-    url = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/{city_id}?Authorization={code}&elementName=WeatherDescription&timeFrom={now}&timeTo={now2}'
-    req = requests.get(url)   # 取得主要縣市預報資料
-    data = req.json()         # json 格式化訊息內容
-    location = data['records']['locations'][0]['location']
-    city = data['records']['locations'][0]['locationsName']
-    for item in location:
-        try:
-            area = item['locationName']
-            note = item['weatherElement'][0]['time'][0]['elementValue'][0]['value']
-            if not f'{city}{area}' in result:
-                result[f'{city}{area}'] = ''
-            else:
-                result[f'{city}{area}'] = result[f'{city}{area}'] + '。\n\n'
-            result[f'{city}{area}'] = result[f'{city}{area}'] + '未來三小時' + note
-        except:
-            pass
-
-    # 空氣品質
-    try:
-        url = 'https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON'
-        req = requests.get(url)
+    url = [f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization={code}',
+           f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization={code}']
+    result = {}
+    for item in url:
+        req = requests.get(item)   # 爬取目前天氣網址的資料
         data = req.json()
-        records = data['records']
-        for item in records:
-            county = item['county']      # 縣市
-            sitename = item['sitename']  # 區域
-            name = f'{county}{sitename}'
-            aqi = int(item['aqi'])       # AQI 數值
-            aqi_status = ['良好','普通','對敏感族群不健康','對所有族群不健康','非常不健康','危害']
-            msg = aqi_status[aqi//50]    # 除以五十之後無條件捨去，取得整數
-
-            for k in result:
-                if name in k:
-                    result[k] = result[k] + f'\n\nAQI：{aqi}，空氣品質{msg}。'
-    except:
-        pass
-
+        station = data['records']['Station']
+        for i in station:
+            city = i['GeoInfo']['CountyName']
+            area = i['GeoInfo']['TownName']
+            address_full = i['Location']
+            if address_full.startswith(address):
+                result[city+area] = {
+                    'weather': i['WeatherElement']['Weather'],
+                    'temp': i['WeatherElement']['AirTemperature'],
+                    'humid': i['WeatherElement']['RelativeHumidity']
+                }
     output = '找不到氣象資訊'
     for i in result:
         if i in address: # 如果地址裡存在 key 的名稱
-            output = f'「{address}」{result[i]}'
+            weather = result[i]['weather'] if result[i]['weather'] else ''
+            temp = result[i]['temp'] if result[i]['temp'] else ''
+            humid = result[i]['humid'] if result[i]['humid'] else ''
+            output = f'「{address}」的天氣狀況「{weather}」，溫度 {temp} 度，相對濕度 {humid}%'
             break
     return output
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -196,9 +144,8 @@ def handle_message(event):
     elif message == '天氣':
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='請輸入你的位置資訊，例如：高雄市前鎮區一心二路'))
     else:
-        # Assume the message contains the location information
         address = message
-        reply = weather(address)
+        reply = check(address)
         text_message = TextSendMessage(text=reply)
         line_bot_api.reply_message(event.reply_token, text_message)
 
