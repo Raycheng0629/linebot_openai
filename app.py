308251c9d8805af411d34a06afca5eb004007041
Raycheng0629 <148304144+Raycheng0629@users.noreply.github.com>
2024-05-10 20:55:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,78 +1,117 @@
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+import requests
 import os
-import json
-from linebot import LineBotApi, WebhookHandler
-import os
-import json
-from linebot import LineBotApi, WebhookHandler
-from linebot.models import (
-    TextSendMessage, StickerSendMessage, ImageSendMessage, VideoSendMessage,
-    AudioSendMessage, LocationSendMessage, TemplateSendMessage, CarouselTemplate,
-    CarouselColumn, ButtonsTemplate, PostbackAction, URIAction, MessageAction,
-    ImageCarouselTemplate, ImageCarouselColumn, QuickReply, QuickReplyButton,
-    ConfirmTemplate, CameraAction, CameraRollAction, LocationAction,
-    DatetimePickerAction, FlexSendMessage, BubbleContainer, BoxComponent,
-    TextComponent, ImageComponent, ImagemapSendMessage, BaseSize,
-    URIImagemapAction, MessageImagemapAction, ImagemapArea
-) # 看需要用到甚麼再import甚麼
+import random
+app = Flask(__name__)
 
-token = os.getenv('CHANNEL_ACCESS_TOKEN'')
-secret = os.getenv('CHANNEL_SECRET')
 
-line_bot_api = LineBotApi(token)
-handler = WebhookHandler(secret)
+# Channel Access Token
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+# Channel Secret
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
-def handle_text_message(msg):
-elif msg == '今日運勢':
-        return TextSendMessage(
-            text='選擇一個動作',
-            quick_reply=QuickReply(
-                items=[
-                    QuickReplyButton(
-                        action=CameraAction(label='晴天'),
-                        image_url='https://storage.googleapis.com/你的icon連結.png'
-                    ),
-                    QuickReplyButton(
-                        action=CameraRollAction(label='雨天'),
-                        image_url='https://storage.googleapis.com/你的icon連結.png'
-                    ),
-                    QuickReplyButton(
-                        action=LocationAction(label='多雲'),
-                        image_url='https://storage.googleapis.com/你的icon連結.png'
-                    ),
-                    QuickReplyButton(
-                        action=PostbackAction(label='陰天', data='action=buy&itemid=123'),
-                        image_url='https://storage.googleapis.com/你的icon連結.png'
-                    ),
-                    QuickReplyButton(
-                        action=MessageAction(label='晴時多雲', text='訊息內容'),
-                        image_url='https://storage.googleapis.com/你的icon連結.png'
-                    )
-                ]
-            )
-        )
-
-def linebot(request):
-    body = request.get_data(as_text=True)
-    json_data = json.loads(body)
-
+def earth_quake():
+    result = []
+    code = 'CWA-B683EE16-4F0D-4C8F-A2AB-CCCA415C60E1'
     try:
-        signature = request.headers['X-Line-Signature']
-        handler.handle(body, signature)
+        # 小區域 https://opendata.cwa.gov.tw/dataset/earthquake/E-A0016-001
+        url = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0016-001?Authorization={code}'
+        req1 = requests.get(url)  # 爬取資料
+        data1 = req1.json()       # 轉換成 json
+        eq1 = data1['records']['Earthquake'][0]           # 取得第一筆地震資訊
+        t1 = data1['records']['Earthquake'][0]['EarthquakeInfo']['OriginTime']
+        # 顯著有感 https://opendata.cwa.gov.tw/dataset/all/E-A0015-001
+        url2 = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0015-001?Authorization={code}'
+        req2 = requests.get(url2)  # 爬取資料
+        data2 = req2.json()        # 轉換成 json
+        eq2 = data2['records']['Earthquake'][0]           # 取得第一筆地震資訊
+        t2 = data2['records']['Earthquake'][0]['EarthquakeInfo']['OriginTime']
 
-        event = json_data['events'][0]
-        tk = event['replyToken']
-        msg_type = event['message']['type']
-
-        if msg_type == 'text':
-            msg = event['message']['text']
-            reply_msg = handle_text_message(msg)
-            line_bot_api.reply_message(tk, reply_msg)
-        else:
-            reply_msg = TextSendMessage(text='你傳的不是文字訊息呦')
-            line_bot_api.reply_message(tk, reply_msg)
-
+        result = [eq1['ReportContent'], eq1['ReportImageURI']] # 先使用小區域地震
+        if t2>t1:
+          result = [eq2['ReportContent'], eq2['ReportImageURI']] # 如果顯著有感地震時間較近，就用顯著有感地震
     except Exception as e:
-        detail = e.args[0]
-        print(detail)
+        print(e)
+        result = ['抓取失敗...','']
+    return result
 
-    return 'OK'
+def weather(address):
+    try:
+        code = 'CWA-B683EE16-4F0D-4C8F-A2AB-CCCA415C60E1'
+        url = [f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization={code}',
+            f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization={code}']
+        result = {}
+        for item in url:
+            req = requests.get(item)   # 爬取目前天氣網址的資料
+            data = req.json()
+            station = data['records']['Station']   # 觀測站
+            for i in station:
+                city = i['GeoInfo']['CountyName']  # 縣市
+                area = i['GeoInfo']['TownName']    # 區域
+                # 使用「縣市+區域」作為 key，例如「高雄市前鎮區」就是 key
+                # 如果 result 裡沒有這個 key，就記錄相關資訊
+                if not f'{city}{area}' in result:
+                    weather = i['WeatherElement']['Weather']
+                    temp = i['WeatherElement']['AirTemperature']
+                    humid = i['WeatherElement']['RelativeHumidity']
+                    # 回傳結果
+                    result[f'{city}{area}'] = f'目前天氣狀況「{weather}」，溫度 {temp} 度，相對濕度 {humid}%！'
+
+        output = '找不到氣象資訊'
+        for i in result:
+            if i in address: # 如果地址裡存在 key 的名稱
+                output = f'「{address}」{result[i]}'
+                break
+    except Exception as e:
+        print(e)
+        output = '抓取失敗...'
+    return output
+
+access_token = '5P7RUl8NwthjuSPZvo8eGR0XNToFXesvPkBcJdpmvHab2pgus1PenUaO43WfO4U+hO/tzsLZHm+h+dpayZ5cwVh4XwBS+OZeB/lGwSxpoP04000GM4FcdU6553pPue5KGa/HIqybLn/OcSA/wNEfxgdB04t89/1O/w1cDnyilFU='
+channel_secret = '82834ecbcd9989b5e1723a193c85ee72'
+
+@app.route("/", methods=['POST'])
+def linebot():
+    body = request.get_data(as_text=True)                    # 取得收到的訊息內容
+    try:
+        line_bot_api = LineBotApi(access_token)     # 確認 token 是否正確
+        handler = WebhookHandler(channel_secret)    # 確認 secret 是否正確
+        signature = request.headers['X-Line-Signature']             # 加入回傳的 headers
+        handler.handle(body, signature)      # 綁定訊息回傳的相關資訊
+        json_data = json.loads(body)         # 轉換內容為 json 格式
+        reply_token = json_data['events'][0]['replyToken']    # 取得回傳訊息的 Token ( reply message 使用 )
+        user_id = json_data['events'][0]['source']['userId']  # 取得使用者 ID ( push message 使用 )
+        print(json_data)                                      # 印出內容
+        type = json_data['events'][0]['message']['type']
+        if type == 'text':
+            text = json_data['events'][0]['message']['text']
+            if text == '雷達回波圖' or text == '雷達回波':
+                line_bot_api.push_message(user_id, TextSendMessage(text='馬上找給你！抓取資料中....'))
+                img_url = f'https://cwaopendata.s3.ap-northeast-1.amazonaws.com/Observation/O-A0058-001.png?{time.time_ns()}'
+                img_message = ImageSendMessage(original_content_url=img_url, preview_image_url=img_url)
+                line_bot_api.reply_message(reply_token,img_message)
+            elif text == '地震':
+                line_bot_api.push_message(user_id, TextSendMessage(text='馬上找給你！抓取資料中....'))
+                reply = earth_quake()
+                text_message = TextSendMessage(text=reply[0])
+                line_bot_api.reply_message(reply_token,text_message)
+                line_bot_api.push_message(user_id, ImageSendMessage(original_content_url=reply[1], preview_image_url=reply[1]))
+            else:
+                text_message = TextSendMessage(text=text)
+                line_bot_api.reply_message(reply_token,text_message)
+        elif type == 'location':
+            line_bot_api.push_message(user_id, TextSendMessage(text='馬上找給你！抓取資料中....'))
+            address = json_data['events'][0]['message']['address'].replace('台','臺')  # 取出地址資訊，並將「台」換成「臺」
+            reply = weather(address)
+            text_message = TextSendMessage(text=reply)
+            line_bot_api.reply_message(reply_token,text_message)
+    except Exception as e:
+        print(e)
+    return 'OK'  
