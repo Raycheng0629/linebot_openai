ba340f87e8fa1e6619650c5de04d283df5c06ba9
Raycheng0629 <148304144+Raycheng0629@users.noreply.github.com>
2024-05-22 17:15:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
 news_categories = {
-    "精選": "https://udn.com/search/tagging/2/%E6%A5%B5%E7%AB%AF%E6%B0%A3%E5%80%99",
+    "精選": "https://udn.com/news/breaknews/1"
     "財經": "https://udn.com/news/breaknews/1/6#breaknews",
     "股市": "https://udn.com/news/breaknews/1/11#breaknews",
     "科技": "https://udn.com/news/breaknews/1/13#breaknews",
@@ -198,7 +198,7 @@ def callback():
             elif text == '即時新聞':
                 quick_reply = QuickReply(
                     items=[
-                        QuickReplyButton(action=MessageAction(label='精選', text='精選')),
+                        QuickReplyButton(action=MessageAction(label='精選', text='精選'))
                         QuickReplyButton(action=MessageAction(label='財經', text='財經')),
                         QuickReplyButton(action=MessageAction(label='股市', text='股市')),
                         QuickReplyButton(action=MessageAction(label='娛樂', text='娛樂')),
