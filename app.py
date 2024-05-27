88753234354b61200e08236f903b9190e3e76167
Raycheng0629 <148304144+Raycheng0629@users.noreply.github.com>
2024-05-27 22:01:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#5/22版本 天氣預報 今日運勢 地震 即時新聞 氣象新聞 功能已完成
 from flask import Flask, request, abort
 import os
 import time
@@ -11,17 +12,25 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, LocationMessage, QuickReply, QuickReplyButton, MessageAction, ImageCarouselTemplate, ImageCarouselColumn, TemplateSendMessage, URIAction
 
 
+
+
 app = Flask(__name__)
+
+
 
 
 # Initialize Google Maps API client
 gmaps = googlemaps.Client(key=os.getenv('GOOGLE_MAPS_API_KEY'))
 
 
+
+
 # Channel Access Token
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 # Channel Secret
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+
+
 
 
 news_categories = {
@@ -31,15 +40,23 @@ news_categories = {
     "科技": "https://udn.com/news/breaknews/1/13#breaknews",
     "娛樂": "https://udn.com/news/breaknews/1/8#breaknews",
     "社會": "https://udn.com/news/breaknews/1/2#breaknews",
-    "氣象新聞": "https://udn.com/search/tagging/2/%E6%A5%B5%E7%AB%AF%E6%B0%A3%E5%80%99"
+    "氣象新聞":"https://udn.com/search/tagging/2/%E6%A5%B5%E7%AB%AF%E6%B0%A3%E5%80%99"
 }
+
+
 
 
 def get_news(url):
     response = requests.get(url)
     response.encoding = 'utf-8'  # 設定編碼避免亂碼
 
+
+
+
     news_list = []
+
+
+
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -59,18 +76,25 @@ def get_news(url):
             except Exception as e:
                 print("連結提取失敗:", str(e))
        
-        return news_list[:5]  # 只取前五筆新聞
+        return news_list
     else:
         print("無法取得網頁內容，狀態碼:", response.status_code)
         return []
+
+
 
 
 def get_extreme_weather_news():
-    url = "https://udn.com/search/tagging/2/%E6%A5%B5%E7%AB%AF%E6%B0%A3%E5%80%99"
-    response = requests.get(url)
+    requests.get("https://udn.com/search/tagging/2/%E6%A5%B5%E7%AB%AF%E6%B0%A3%E5%80%99")
     response.encoding = 'utf-8'  # 設定編碼避免亂碼
 
+
+
+
     news_list = []
+
+
+
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -83,17 +107,15 @@ def get_extreme_weather_news():
                     link_text = link_element.get_text(strip=True)
                     link_url = link_element['href']
                     absolute_link_url = urljoin(url, link_url)  # 轉換為絕對路徑
-                    news_list.append({
-                        'title': link_text,
-                        'url': absolute_link_url
-                    })
             except Exception as e:
                 print("連結提取失敗:", str(e))
        
-        return news_list[:5]  # 只取前五筆新聞
+        return news_list
     else:
         print("無法取得網頁內容，狀態碼:", response.status_code)
         return []
+
+
 
 
 def earth_quake():
@@ -106,11 +128,17 @@ def earth_quake():
         eq1 = data1['records']['Earthquake'][0]
         t1 = data1['records']['Earthquake'][0]['EarthquakeInfo']['OriginTime']
 
+
+
+
         url2 = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0015-001?Authorization={code}'
         req2 = requests.get(url2)
         data2 = req2.json()
         eq2 = data2['records']['Earthquake'][0]
         t2 = data2['records']['Earthquake'][0]['EarthquakeInfo']['OriginTime']
+
+
+
 
         result = [eq1['ReportContent'], eq1['ReportImageURI']]
         if t2 > t1:
@@ -121,10 +149,11 @@ def earth_quake():
     return result
 
 
+
+
 def weather(address):
     result = {}
     code = 'CWA-B683EE16-4F0D-4C8F-A2AB-CCCA415C60E1'
-    
     # 即時天氣
     try:
         url = [
@@ -138,14 +167,17 @@ def weather(address):
             for i in station:
                 city = i['GeoInfo']['CountyName']
                 area = i['GeoInfo']['TownName']
-                if f'{city}{area}' not in result:
+                if not f'{city}{area}' in result:
                     weather = i['WeatherElement']['Weather']
                     temp = i['WeatherElement']['AirTemperature']
                     humid = i['WeatherElement']['RelativeHumidity']
                     result[f'{city}{area}'] = f'目前天氣狀況「{weather}」，溫度 {temp} 度，相對濕度 {humid}%！'
     except Exception as e:
         print(e)
-    
+
+
+
+
     api_list = {
         "宜蘭縣": "F-D0047-001", "桃園市": "F-D0047-005", "新竹縣": "F-D0047-009", "苗栗縣": "F-D0047-013",
         "彰化縣": "F-D0047-017", "南投縣": "F-D0047-021", "雲林縣": "F-D0047-025", "嘉義縣": "F-D0047-029",
@@ -161,7 +193,10 @@ def weather(address):
             break
     if not city_id:
         return '找不到氣象資訊'
-    
+
+
+
+
     t = time.time()
     t1 = time.localtime(t + 28800)
     t2 = time.localtime(t + 28800 + 10800)
@@ -176,7 +211,7 @@ def weather(address):
         try:
             area = item['locationName']
             note = item['weatherElement'][0]['time'][0]['elementValue'][0]['value']
-            if f'{city}{area}' not in result:
+            if not f'{city}{area}' in result:
                 result[f'{city}{area}'] = ''
             else:
                 result[f'{city}{area}'] += '。\n\n'
@@ -184,27 +219,44 @@ def weather(address):
         except Exception as e:
             print(e)
 
-    rain_info = 0
+
+
+
     try:
-        for i in result:
-            if i in address:
-                output = f'「{address}」{result[i]}'
-                if "降雨機率" in result[i]:
-                    rain_info = float(result[i].split("降雨機率")[1].split(" ")[0])
-                break
+        url = 'https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON'
+        req = requests.get(url)
+        data = req.json()
+        records = data['records']
+        aqi_status = ['良好', '普通', '對敏感族群不健康', '對所有族群不健康', '非常不健康', '危害']
+        for item in records:
+            county = item['county']
+            sitename = item['sitename']
+            name = f'{county}{sitename}'
+            aqi = int(item['aqi'])
+            msg = aqi_status[aqi // 50]
+
+
+
+
+            for k in result:
+                if name in k:
+                    result[k] += f'\n\nAQI：{aqi}，空氣品質{msg}。'
     except Exception as e:
         print(e)
-    
-    try:
-        if rain_info > 40:
-            output += '，請攜帶雨具'
-    except Exception as e:
-        print(e)
-        return output, rain_info
-
-    return output, rain_info
 
 
+
+
+    for i in result:
+        if i in address:
+            output = f'「{address}」{result[i]}'
+            break
+    return output
+
+
+
+
+@app.route("/callback", methods=['POST'])
 @app.route("/callback", methods=['POST'])
 def callback():
     body = request.get_data(as_text=True)
@@ -224,11 +276,11 @@ def callback():
                 line_bot_api.reply_message(reply_token, img_message)
             elif text in news_categories:
                 news_data = get_news(news_categories[text])
-                news_message = "\n\n".join([f"{i+1}. {news['title']}\n{news['url']}" for i, news in enumerate(news_data)])
+                news_message = "\n\n".join([f"{i+1}. {news['title']}\n{news['url']}" for i, news in enumerate(news_data[:5])])
                 line_bot_api.reply_message(reply_token, TextSendMessage(text=news_message))
             elif text == '氣象新聞':
                 news_data = get_news(news_categories[text])
-                news_message = "\n\n".join([f"{i+1}. {news['title']}\n{news['url']}" for i, news in enumerate(news_data)])
+                news_message = "\n\n".join([f"{i+1}. {news['title']}\n{news['url']}" for i, news in enumerate(news_data[:5])])
                 line_bot_api.reply_message(reply_token, TextSendMessage(text=news_message))
             elif text == '即時新聞':
                 quick_reply = QuickReply(
@@ -242,26 +294,11 @@ def callback():
                     ]
                 )
                 line_bot_api.reply_message(reply_token, TextSendMessage(text="請選擇新聞類別", quick_reply=quick_reply))
-            elif text == '今日運勢':
-                line_bot_api.reply_message(reply_token, TextSendMessage(
-                    text='請選擇你那邊的天氣狀況',
-                    quick_reply=QuickReply(
-                        items=[
-                            QuickReplyButton(action=MessageAction(label='晴天', text='晴天')),
-                            QuickReplyButton(action=MessageAction(label='晴時多雲', text='晴時多雲')),
-                            QuickReplyButton(action=MessageAction(label='雨天', text='雨天')),
-                            QuickReplyButton(action=MessageAction(label='陰天', text='陰天')),
-                            QuickReplyButton(action=MessageAction(label='多雲', text='多雲'))
-                        ]
-                    )
-                ))
         elif type == 'location':
             address = json_data['events'][0]['message']['address'].replace('台', '臺')
-            reply, rain_info = weather(address)
+            reply = weather(address)
             text_message = TextSendMessage(text=reply)
             line_bot_api.reply_message(reply_token, text_message)
-            if rain_info > 40:
-                line_bot_api.push_message(user_id, TextSendMessage(text="請攜帶雨具"))
     except InvalidSignatureError:
         abort(400)
     except Exception as e:
@@ -269,6 +306,60 @@ def callback():
     return 'OK'
 
 
+
+
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_location(event):
+    latitude = event.message.latitude
+    longitude = event.message.longitude
+    geocode_result = gmaps.reverse_geocode((latitude, longitude), language='zh-TW')
+    address = geocode_result[0]['formatted_address'].replace('台', '臺')
+    weather_forecast = weather(address)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=weather_forecast)
+    )
+
+
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = event.message.text
+    if "天氣預報" in message:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="請傳送你所在的位置資訊")
+        )
+    elif message == '今日運勢':
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(
+            text='請選擇你那邊的天氣狀況',
+            quick_reply=QuickReply(
+                items=[
+                    QuickReplyButton(action=MessageAction(label='晴天', text='晴天')),
+                    QuickReplyButton(action=MessageAction(label='晴時多雲', text='晴時多雲')),
+                    QuickReplyButton(action=MessageAction(label='雨天', text='雨天')),
+                    QuickReplyButton(action=MessageAction(label='陰天', text='陰天')),
+                    QuickReplyButton(action=MessageAction(label='多雲', text='多雲'))
+                ]
+            )
+        ))
+    elif message in ['晴天', '晴時多雲', '雨天', '陰天', '多雲']:
+        weather_info = message
+        fortune = choose_fortune(weather_info)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=fortune))
+    elif message == '地震':
+        reply = earth_quake()
+        text_message = TextSendMessage(text=reply[0])
+        line_bot_api.reply_message(event.reply_token, text_message)
+        line_bot_api.push_message(event.source.user_id, ImageSendMessage(original_content_url=reply[1], preview_image_url=reply[1]))
+
+
+
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+
+
