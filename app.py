b9d4fd9cccf0540621c08a2fad3bc785c2410fe4
Raycheng0629 <148304144+Raycheng0629@users.noreply.github.com>
2024-05-30 02:48:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -284,87 +284,87 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text="輸入:我想吃 輸入欲查詢的地址,如:我想吃 台北市士林區臨溪路70號")
         )
-         if lineMessage[0:3] == "我想吃":
-            lineMes = lineMessage
-            if lineMes[4:].strip() == '':
-                line_bot_api.reply_message(event.reply_token, TextSendMessage(text="查無該地址"))
-                return
+    elif message.startswith("我想吃"):
+        lineMes = message
+        if lineMes[3:].strip() == '':
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="查無該地址"))
+            return
+        else:
+            address = lineMes[3:].strip()
+        
+        addurl = 'https://maps.googleapis.com/maps/api/geocode/json?key={}&address={}&sensor=false'.format(os.getenv('GOOGLE_MAPS_API_KEY'), address)
+        addressReq = requests.get(addurl)
+        addressDoc = addressReq.json()
+        
+        if not addressDoc['results']:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="查無地址"))
+            return
+        
+        lat = addressDoc['results'][0]['geometry']['location']['lat']
+        lng = addressDoc['results'][0]['geometry']['location']['lng']
+        
+        foodStoreSearch = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={}&location={},{}&rankby=distance&type=restaurant&language=zh-TW".format(os.getenv('GOOGLE_MAPS_API_KEY'), lat, lng)
+        foodReq = requests.get(foodStoreSearch)
+        nearby_restaurants_dict = foodReq.json()
+        top20_restaurants = nearby_restaurants_dict["results"]
+        
+        if not top20_restaurants:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="查無餐廳"))
+            return
+        
+        bravo = []
+        
+        for restaurant in top20_restaurants:
+            try:
+                if restaurant['rating'] > 3.8:
+                    bravo.append(restaurant)
+                    if len(bravo) >= 8:
+                        break
+            except KeyError:
+                continue
+        
+        if not bravo:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="沒找到高評分的店家，隨便選一家"))
+            return
+        
+        columns = []
+        for restaurant in bravo:
+            if restaurant.get("photos"):
+                photo_reference = restaurant["photos"][0]["photo_reference"]
+                photo_width = restaurant["photos"][0]["width"]
+                thumbnail_image_url = "https://maps.googleapis.com/maps/api/place/photo?key={}&photoreference={}&maxwidth={}".format(os.getenv('GOOGLE_MAPS_API_KEY'), photo_reference, photo_width)
             else:
-                address = lineMes[4:].strip()
+                thumbnail_image_url = None
+
+            rating = "無" if restaurant.get("rating") is None else restaurant["rating"]
+            address = "沒有資料" if restaurant.get("vicinity") is None else restaurant["vicinity"]
+            details = "Google Map分數: {}\n地址: {}".format(rating, address)
             
-            addurl = 'https://maps.googleapis.com/maps/api/geocode/json?key={}&address={}&sensor=false'.format(os.getenv('GOOGLE_MAPS_API_KEY'), address)
-            addressReq = requests.get(addurl)
-            addressDoc = addressReq.json()
-            
-            if not addressDoc['results']:
-                line_bot_api.reply_message(event.reply_token, TextSendMessage(text="查無地址"))
-                return
-            
-            lat = addressDoc['results'][0]['geometry']['location']['lat']
-            lng = addressDoc['results'][0]['geometry']['location']['lng']
-            
-            foodStoreSearch = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={}&location={},{}&rankby=distance&type=restaurant&language=zh-TW".format(os.getenv('GOOGLE_MAPS_API_KEY'), lat, lng)
-            foodReq = requests.get(foodStoreSearch)
-            nearby_restaurants_dict = foodReq.json()
-            top20_restaurants = nearby_restaurants_dict["results"]
-            
-            if not top20_restaurants:
-                line_bot_api.reply_message(event.reply_token, TextSendMessage(text="查無餐廳"))
-                return
-            
-            bravo = []
-            
-            for restaurant in top20_restaurants:
-                try:
-                    if restaurant['rating'] > 3.8:
-                        bravo.append(restaurant)
-                        if len(bravo) >= 8:
-                            break
-                except KeyError:
-                    continue
-            
-            if not bravo:
-                line_bot_api.reply_message(event.reply_token, TextSendMessage(text="沒找到高評分的店家，隨便選一家"))
-                return
-            
-            columns = []
-            for restaurant in bravo:
-                if restaurant.get("photos"):
-                    photo_reference = restaurant["photos"][0]["photo_reference"]
-                    photo_width = restaurant["photos"][0]["width"]
-                    thumbnail_image_url = "https://maps.googleapis.com/maps/api/place/photo?key={}&photoreference={}&maxwidth={}".format(os.getenv('GOOGLE_MAPS_API_KEY'), photo_reference, photo_width)
-                else:
-                    thumbnail_image_url = None
-    
-                rating = "無" if restaurant.get("rating") is None else restaurant["rating"]
-                address = "沒有資料" if restaurant.get("vicinity") is None else restaurant["vicinity"]
-                details = "Google Map分數: {}\n地址: {}".format(rating, address)
-                
-                map_url = "https://www.google.com/maps/search/?api=1&query={},{}&query_place_id={}".format(
-                    restaurant["geometry"]["location"]["lat"],
-                    restaurant["geometry"]["location"]["lng"],
-                    restaurant["place_id"]
-                )
-                
-                column = CarouselColumn(
-                    thumbnail_image_url=thumbnail_image_url,
-                    title=restaurant["name"],
-                    text=details,
-                    actions=[
-                        URITemplateAction(
-                            label='查看地圖',
-                            uri=map_url
-                        )
-                    ]
-                )
-                columns.append(column)
-            
-            carousel_template = TemplateSendMessage(
-                alt_text='高評分餐廳',
-                template=CarouselTemplate(columns=columns)
+            map_url = "https://www.google.com/maps/search/?api=1&query={},{}&query_place_id={}".format(
+                restaurant["geometry"]["location"]["lat"],
+                restaurant["geometry"]["location"]["lng"],
+                restaurant["place_id"]
             )
             
-            line_bot_api.reply_message(event.reply_token, carousel_template)
+            column = CarouselColumn(
+                thumbnail_image_url=thumbnail_image_url,
+                title=restaurant["name"],
+                text=details,
+                actions=[
+                    URITemplateAction(
+                        label='查看地圖',
+                        uri=map_url
+                    )
+                ]
+            )
+            columns.append(column)
+        
+        carousel_template = TemplateSendMessage(
+            alt_text='高評分餐廳',
+            template=CarouselTemplate(columns=columns)
+        )
+        
+        line_bot_api.reply_message(event.reply_token, carousel_template)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
